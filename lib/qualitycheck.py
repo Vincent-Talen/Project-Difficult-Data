df439795b8841592f5d3e83492f5d3381b6cf6a6
Rob Meulenkamp <lrmeulenkamp@bioinf.nl>
2020-12-02 11:34:56 +0100

    added first version qualitycheck.py

--- lib/qualitycheck.py
+++ lib/qualitycheck.py
@@ -1,32 +1,42 @@
 #!/usr/bin/env python3
 
 """
-Module
+execute quality check for every fastq.gz file in the directory.
 """
 
-# METADATA VARIABLES
-__author__ = "Vincent Talen"
-__status__ = "Template"
+__author__ = "Rob Meulenkamp"
+__status__ = "template"
 __date__ = "02-12-2020"
-__version__ = "v0.1"
+__version__ = "v1"
 
-# IMPORTS
+
+import os
 import sys
+import argparse
 
 
-class QualityCheck:
-    """"""
-    def check_quality(self):
-        pass
+def fastqc(fastq_dir, fastqc_dir):
+    """prepared the data for the fastqc tool."""
+
+    query = 'fastqc --casava {}*fastq.gz -o={}'.format(fastq_dir, fastqc_dir)
+                                                            # 'fastq_dir' directory waar de *fastq.gz staan.
+    os.system(query)                                        # '-o=' plek van output directory.
+
+    return 0
 
 
-# MAIN
-def main():
-    """Main function calling forth all tasks"""
-    q_check = QualityCheck()
-    q_check.check_quality()
+def main(args):
+    """execute the programm"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("fastq", help="Give the directory to the fastq.gz files.")
+    parser.add_argument("fastqc", help="Give the output directory to the fastqc report(s).")
+    arg = parser.parse_args()
+    fastq_dir = arg.fastq
+    fastqc_dir = arg.fastqc
+    fastqc(fastq_dir, fastqc_dir)
+
     return 0
 
 
 if __name__ == "__main__":
-    sys.exit(main())
+    sys.exit(main(sys.argv))
