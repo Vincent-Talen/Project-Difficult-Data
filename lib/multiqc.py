ddd99a67a0efc582f63308ea0c40b86bb38cf721
Joost Numan <jrnuman@bioinf.nl>
2020-12-11 15:11:37 +0100

    created the multiqc module

--- lib/multiqc.py
+++ lib/multiqc.py
@@ -1,31 +1,39 @@
 #!/usr/bin/env python3
 
 """
-Module
+This python module runs the multiQC tool, it requires the target output directory location.
+This module searches the entire output directory for other tool results
+and uses them to create an html result file.
+The results will be written to the outputdirectory/Results/multiQC directory
 """
 
+
 # METADATA VARIABLES
-__author__ = "Vincent Talen"
-__status__ = "Template"
-__date__ = "02-12-2020"
-__version__ = "v0.1"
+__author__ = "Joost Numan"
+__status__ = "Development"
+__date__ = "09-12-2020"
+__version__ = "v0.2"
+
 
 # IMPORTS
 import sys
+import subprocess
 
 
-class RunMultiQC:
-    """"""
-    def perform_multiqc(self):
-        pass
+# FUNCTIONS
+def perform_multiqc(outputdir):
+    """
+    this function creates a query for running the multiQC tool
+    the query is run in the terminal by using subprocess.run()
+    """
+    query = "multiqc {} -o {}Results/multiQC".format(outputdir, outputdir)
+    subprocess.run(query.split())
 
 
 # MAIN
 def main():
     """Main function calling forth all tasks"""
-    multiqc = RunMultiQC()
-    multiqc.perform_multiqc()
-    return 0
+    perform_multiqc(".")
 
 
 if __name__ == "__main__":
