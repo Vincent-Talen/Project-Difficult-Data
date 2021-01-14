9d04878f859569d86bb03c11e92f9a73e6e71359
Vincent Talen <v.k.talen@st.hanze.nl>
2021-01-14 22:52:51 +0100

    Updated and modified the count_matrix module to be fully working

--- lib/count_matrix.py
+++ lib/count_matrix.py
@@ -1,42 +1,47 @@
 #!/usr/bin/env python3
 
 """
-this python module runs the featureCounts tool
-it requires the output directory and an annotation file
-the result is a txt file
+This module contains the create_count_matrix function that can be used to create a count matrix for
+one or more bam files.
 """
 
 
 # METADATA VARIABLES
-__author__ = "Joost Numan"
+__author__ = "Joost Numan and Vincent Talen"
 __status__ = "Development"
-__date__ = "8-01-2021"
-__version__ = "v0.2"
+__date__ = "14-01-2021"
+__version__ = "v0.3.2"
 
 
 # IMPORTS
 import sys
-import subprocess
+import glob
+from subprocess import run
 
 
 # FUNCTIONS
-def create_count_matrix(outputdir, anno_file, bam_file):
+def create_count_matrix(output_dir):
     """
-    this function creates a query to be run with subprocess to run the Feature Counts tool
-    the input for this function is outputdir and anno_file
-    outputdir is the directory where this script needs to be run
-    anno_file is the annotation file in GTF format
-    bam_file is the Bam file required as input for the tool
+    This function creates and runs a query that calls forth the Feature Counts tool
+    from the Subread package.
+
+    :param output_dir: is the directory where this script needs to be run
     """
-    query = "{}/featureCounts/bin/featureCounts -a {} -o {}/RawData/counts/geneCounts.txt {}/Preprocessing/markDuplicates/{}"
-    query = query.format(outputdir, anno_file, outputdir, outputdir, bam_file)
-    subprocess.run(query.split())
+    if output_dir.startswith("/"):
+        output_dir = output_dir[1:]
+
+    files = glob.glob(f"{output_dir}/Preprocessing/markDuplicates/*_sorted.bam")
+    anno_file = f"{output_dir}/Data/genome/Homo_sapiens.GRCh38.84.gtf"
+
+    query = ["lib/Subread-2.0.1/bin/featureCounts", "-a", anno_file,
+             "-o", f"{output_dir}/RawData/counts/counts.txt", *files]
+    run(query)
 
 
 # MAIN
 def main():
-    """Main function calling forth all tasks"""
-    create_count_matrix(".", "Homo_sapiens.GRCh38.84.gtf")
+    """Main function to test the module"""
+    create_count_matrix("/output")
     return 0
 
 
